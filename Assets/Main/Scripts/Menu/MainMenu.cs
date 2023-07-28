f32a82cc9a7664f728b88038194f9c492e5b3ce9
Lukas Nydegger <nydegger.lukas@gmail.com>
2023-07-28 16:11:03 +0200

    add mp

---
+++ Assets/Main/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button joinButton;
+    [SerializeField] private TMP_InputField ipInput;
+    [SerializeField] private SceneAsset gameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hostButton.onClick.AddListener(HostGame);
+        joinButton.onClick.AddListener(JoinGame);
+    }
+
+    private void JoinGame()
+    {
+        Debug.Log(ipInput.text);
+        //get ip input and start client
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
+            ipInput.text,  // The IP address is a string
+            (ushort)12345 // The port number is an unsigned short
+        );
+        NetworkManager.Singleton.StartClient();
+    }
+
+    private void HostGame()
+    {
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
+            "127.0.0.1",  // The IP address is a string
+            (ushort)12345, // The port number is an unsigned short
+            "0.0.0.0" // The server listen address is a string.
+        );
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(gameScene.name, LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Main/Scripts/Movement.cs
+++ Assets/Main/Scripts/Movement.cs
@@ -1,99 +1,54 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using Main.Scripts;
 using Main.Scripts.Helpers;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-//physic based movement class
-//this class jumps and moves the player based on rigidbody 3d physics
-public class Movement : MonoBehaviour
+namespace Main.Scripts
 {
-    //variables
-    [SerializeField] private float jumpForceUp = 5f;
-    [SerializeField] private float jumpForceForward = 5f;
-    
-    //input system
-    private Vector2 _move;
-
-    private bool _isGrounded = true;
-    private Throttle _projectionThrottle = new Throttle();
-    private Vector2 _startPosition = Vector2.zero;
-    private float _normalizationFactor;
-
-    private Rigidbody rb;
-    private Projection _projection;
-
-    private void Start()
+    public class Movement : NetworkBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-        _projection = GetComponent<Projection>();
-        _normalizationFactor = Math.Min(Screen.width / 4f, Screen.height / 4f);
-        Debug.Log(_normalizationFactor);
-    }
+        //variables
+        [SerializeField] private float jumpForceUp = 5f;
+        [SerializeField] private float jumpForceForward = 5f;
 
-    private void OnCollisionEnter(Collision other)
-    {
-        //todo: check if collision is ground
-        _isGrounded = true;
-        rb.velocity = Vector3.zero;
-        rb.angularVelocity = Vector3.zero;
-    }
+        //input system
+        private Vector2 _move;
 
-    private void OnMove(InputValue value)
-    {
-        if(!_isGrounded) return;
-        _move = value.Get<Vector2>();
-        if (_move.magnitude > 0.1f)
+        private bool _isGrounded = true;
+        private Throttle _projectionThrottle = new Throttle();
+        private Vector2 _startPosition = Vector2.zero;
+        private float _normalizationFactor;
+
+        private Rigidbody rb;
+        private Projection _projection;
+
+        void Start()
         {
-            _projectionThrottle.Run(DoTrajectory, 0.05f);
+            rb = GetComponent<Rigidbody>();
+            _projection = GetComponent<Projection>();
+            _normalizationFactor = Math.Min(Screen.width / 4f, Screen.height / 4f);
+            Debug.Log(_normalizationFactor);
         }
-        else
+
+        void FixedUpdate()
         {
-            _projection.HideTrajectory();
+            if(!IsOwner) return;
+            _isGrounded = Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, 0.11f);
         }
-    }
-    
-    private void DoTrajectory()
-    {
-        var transform1 = this.transform;
-        //make a jump force based on the jump force variable player forward and up vector
-        var jumpVelocity = GetJumpVelocity();
-        _projection.CalculatePosition(transform1.position, jumpVelocity);
-    }
 
-    private Vector3 GetJumpVelocity()
-    {
-        var jumpVelocity = Vector3.zero;
-        jumpVelocity += Vector3.forward * _move.y * jumpForceForward;
-        jumpVelocity += Vector3.right * _move.x * jumpForceForward;
-        jumpVelocity += Vector3.up * jumpForceUp;
-        return jumpVelocity;
-    }
-
-    private void OnJump()
-    {
-        DoJump();
-    }
-
-    private void OnMoveByRelease(InputValue value)
-    {
-        if(!_isGrounded) return;
-
-        var pos = value.Get<Vector2>();
-        if (pos != Vector2.zero)
+        private void OnCollisionEnter(Collision other)
         {
-            if (_startPosition == Vector2.zero)
-            {
-                _startPosition = value.Get<Vector2>();
-            }
-            else
-            {
-                _move = (value.Get<Vector2>() - _startPosition) / _normalizationFactor;
-                if(_move.magnitude > 1f) _move.Normalize();
-            }
-        
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        private void OnMove(InputValue value)
+        {
+            if (!_isGrounded) return;
+            if (!IsOwner) return;
+
+            _move = value.Get<Vector2>();
             if (_move.magnitude > 0.1f)
             {
                 _projectionThrottle.Run(DoTrajectory, 0.05f);
@@ -103,35 +58,88 @@ public class Movement : MonoBehaviour
                 _projection.HideTrajectory();
             }
         }
-        else
-        {
-            if (_move.magnitude > 0.1f)
-            {
-                DoJump();
-            }
-            _move = Vector2.zero;
-            _startPosition = Vector2.zero;
-        }
-    }
 
-    private void DoJump()
-    {
-        if (_isGrounded)
+        private void DoTrajectory()
         {
-            _projection.HideTrajectory();
-            _isGrounded = false;
+            var transform1 = this.transform;
+            //make a jump force based on the jump force variable player forward and up vector
             var jumpVelocity = GetJumpVelocity();
-            
-            // Project the jumpVelocity onto the plane defined by the Y-axis
-            Vector3 projectedJumpVelocity = Vector3.ProjectOnPlane(jumpVelocity, Vector3.up);
+            _projection.CalculatePosition(transform1.position, jumpVelocity);
+        }
 
-            // Get the rotation quaternion to look in the direction of the projected jumpVelocity
-            Quaternion rotation = Quaternion.LookRotation(projectedJumpVelocity, Vector3.up);
-            
-            rb.MoveRotation(rotation);
-            rb.AddForce(jumpVelocity, ForceMode.Impulse);
-            //rotate player in direction of jump but only on the x axis
-            
+        private Vector3 GetJumpVelocity()
+        {
+            var jumpVelocity = Vector3.zero;
+            jumpVelocity += Vector3.forward * _move.y * jumpForceForward;
+            jumpVelocity += Vector3.right * _move.x * jumpForceForward;
+            jumpVelocity += Vector3.up * jumpForceUp;
+            return jumpVelocity;
+        }
+
+        private void OnJump()
+        {
+            DoJump();
+        }
+
+        private void OnMoveByRelease(InputValue value)
+        {
+            if (!_isGrounded) return;
+            if (!IsOwner) return;
+
+            var pos = value.Get<Vector2>();
+            if (pos != Vector2.zero)
+            {
+                if (_startPosition == Vector2.zero)
+                {
+                    _startPosition = value.Get<Vector2>();
+                }
+                else
+                {
+                    _move = (value.Get<Vector2>() - _startPosition) / _normalizationFactor;
+                    if (_move.magnitude > 1f) _move.Normalize();
+                }
+
+                if (_move.magnitude > 0.1f)
+                {
+                    _projectionThrottle.Run(DoTrajectory, 0.05f);
+                }
+                else
+                {
+                    _projection.HideTrajectory();
+                }
+            }
+            else
+            {
+                if (_move.magnitude > 0.1f)
+                {
+                    DoJump();
+                }
+
+                _move = Vector2.zero;
+                _startPosition = Vector2.zero;
+            }
+        }
+
+        private void DoJump()
+        {
+            if (!IsOwner) return;
+
+            if (_isGrounded)
+            {
+                _projection.HideTrajectory();
+                _isGrounded = false;
+                var jumpVelocity = GetJumpVelocity();
+
+                // Project the jumpVelocity onto the plane defined by the Y-axis
+                Vector3 projectedJumpVelocity = Vector3.ProjectOnPlane(jumpVelocity, Vector3.up);
+
+                // Get the rotation quaternion to look in the direction of the projected jumpVelocity
+                Quaternion rotation = Quaternion.LookRotation(projectedJumpVelocity, Vector3.up);
+
+                rb.MoveRotation(rotation);
+                rb.AddForce(jumpVelocity, ForceMode.Impulse);
+                //rotate player in direction of jump but only on the x axis
+            }
         }
     }
 }
